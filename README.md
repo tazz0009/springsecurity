ed52a5b9f9df8918e428ca6c8a5a9e550e6ba19a
tazz0009 <tazz0009@gmail.com>
2021-12-11 08:17:09 +0900

    [chap03] chap03. UserDetailsService / IN MEMORY USER DETAILS MANAGER / PASSWORD ENCODING WITH BCRYPT

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ## chap01. Security Config
 
 ## chap02. ANT MATCHERS
+
+## chap03. UserDetailsService / IN MEMORY USER DETAILS MANAGER / PASSWORD ENCODING WITH BCRYPT
--- src/main/java/com/tazz009/springsec/security/ApplicationSecurityConfig.java
+++ src/main/java/com/tazz009/springsec/security/ApplicationSecurityConfig.java
@@ -1,14 +1,28 @@
 package com.tazz009.springsec.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	private final PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+		this.passwordEncoder = passwordEncoder;
+	}
+	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http
@@ -21,11 +35,22 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 				.anyRequest()
 				// 인증된 모든 사용자가 URL을 허용하도록 지정합니다.
 				.authenticated()
-				// AuthorizeHttpRequestsConfigurer를 사용하여 완료되면 
+				// AuthorizeHttpRequestsConfigurer를 사용하여 완료되면
 				// HttpSecurityBuilder를 반환합니다.
 				.and()
 				// HTTP 기본 인증을 구성합니다.
 				.httpBasic();
+	}
+
+	@Override
+	@Bean
+	protected UserDetailsService userDetailsService() {
+		UserDetails user1 = User.builder()
+				.username("tazz009")
+				.password(passwordEncoder.encode("1234qwer"))
+				.roles("STUDENT")
+				.build();
+		return new InMemoryUserDetailsManager(user1);
 	}
 
 }
